feb4c7413622a991360767e64614c443bcaa81e3
Ben Kissi <benjamin.kissi@meltwater.com>
2015-11-25 00:03:30 +0000

    added github URL to package.describe

--- packages/error-toaster/package.js
+++ packages/error-toaster/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Display errors to the user in a cool way',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/benkissi/error-toaster',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
